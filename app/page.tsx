f601f330d910ee7c08b677de74af7833abdfa251
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2024-01-24 22:46:03 +1100

    added product card to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
-const Home = () =>{
-  return(
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
+  return (
     <>
       <section className="px-6 border-2 py-24 ">
         <div className="flex max-xl:flex-col gap-16">
@@ -15,38 +19,30 @@ const Home = () =>{
                 width={16}
                 height={16}
               />
-
             </p>
             <h1 className="head-text">
-              Unleash the Power of  
-              <span className="text-primary"> PriceWise</span> 
-
+              Unleash the Power of
+              <span className="text-primary"> PriceWise</span>
             </h1>
             <p className="mt-6">
-            Powerful, self-serve product and growth 
-            analytics to help you convert, engage, and 
-            retain more.
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
             </p>
-            <Searchbar/>
+            <Searchbar />
           </div>
-          <HeroCarousel/>
-          
-
+          <HeroCarousel />
         </div>
-        
       </section>
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-18">
-          {["Apple IPhone 15", "Book", "Runners"].map((product)=>(
-            <div>
-              {product}
-            </div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
-
       </section>
     </>
-  )
-}
+  );
+};
 export default Home;
+
