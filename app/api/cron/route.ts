008f4d53e61ec0bba78f2d46a600601d3e3e9561
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2024-01-29 17:21:40 +1100

    fixed build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -9,6 +9,13 @@ import {
 } from "@/lib/utils";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { NextResponse } from "next/server";
+
+export const maxDuration = 300;
+
+export const dynamic = "force-dynamic";
+
+export const revalidate = 0;
+
 export async function GET() {
   try {
     connectToDB();
@@ -33,7 +40,7 @@ export async function GET() {
           averagePrice: getAveragePrice(updatedPriceHistory),
         };
         const updatedProduct = await Product.findOneAndUpdate(
-          { url: scrappedProduct.url },
+          { url: product.url },
           product,
         );
         const emailNotifType = getEmailNotifType(
